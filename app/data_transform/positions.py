2ebca1dcf6236e586245df183daa8f09edf86b48
chris_khan <ckhan46.ck@gmail.com>
2025-10-26 17:13:47 +0000

    adding dependencies

--- app/data_transform/positions.py
+++ app/data_transform/positions.py
@@ -1,3 +1,6 @@
+import os
+
+import plotly.express as px
 import polars as pl
 
 year = "2025"
@@ -11,11 +14,20 @@ dfs = []
 for round_number in range(1, len(calendar_df) + 1):
     round_path = f"data/{year}/round_{round_number}/race.json"
 
-    df = pl.read_json(round_path).select(
-        pl.lit(round_number).alias("round_number"),
-        "position",
-        "rider",
-        "points",
+    df = (
+        pl.read_json(round_path)
+        .select(
+            pl.lit(round_number).alias("round_number"),
+            "position",
+            "rider",
+            "points",
+        )
+        .with_columns(
+            pl.when(pl.col("position").is_null())
+            .then(None)  # or len(grid)
+            .otherwise(pl.col("position"))
+            .alias("position")
+        )
     )
 
     dfs.append(df)
@@ -23,3 +35,15 @@ for round_number in range(1, len(calendar_df) + 1):
 all_races = pl.concat(dfs, how="vertical")
 
 all_races = all_races.join(calendar_df, on="round_number", how="inner")
+
+pdf = all_races.to_pandas()
+
+fig = px.line(
+    pdf,
+    x="round_name",
+    y="position",
+    color="rider",
+    markers=True,
+)
+fig.update_yaxes(autorange="reversed")
+fig.show()
